e6de18089c8e90fb1d5c470f9aab8186ecb708d7
Timing <836054933@qq.com>
2019-03-31 20:57:16 +0800

    Add a note.

--- cloud-swagger/src/main/java/com/timing/cloud/swagger/Application.java
+++ cloud-swagger/src/main/java/com/timing/cloud/swagger/Application.java
@@ -10,4 +10,15 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    /*
+
+    项目启动后，
+    访问如下接口，就能看到 swagger 文档
+
+    http://localhost:8080/swagger-ui.html#/user-controller/getUserUsingGET
+
+
+
+     */
 }
